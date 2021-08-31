c2a685a89cf82eb16dba18bdeec6ba1e6fae471f
Alerrandro Janio <alerrandro.janio07@gmail.com>
2021-08-31 19:52:16 -0300

    login

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,8 +7,19 @@ import './css/bootstrap.min.css'
 import './css/bootstrap.min.style.css'
 
 export default {
+  name: "Login",
   
-}
+  data() {
+    return {
+      logged: false,
+    };
+  },
+  
+  methods: {
+
+  },
+
+};
 </script>
 
 
